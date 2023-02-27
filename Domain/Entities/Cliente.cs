28198de0ab8da305be7dfb6c420bf6fb48a71df5
Ignacio Ainol <iggnaxios@gmail.com>
2023-02-26 22:45:20 -0300

    testing redis cache

--- Domain/Entities/Cliente.cs
+++ Domain/Entities/Cliente.cs
@@ -21,6 +21,11 @@ namespace Domain.Entities
                 }
                 return this._edad;
             }
+
+            set
+            {
+                this._edad = value;
+            }
         }
     }
 }
