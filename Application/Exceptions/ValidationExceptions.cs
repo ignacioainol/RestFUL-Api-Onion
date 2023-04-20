6bc44e43eb581900d11bc24a48e644564c89b52d
Ignacio Ainol <iggnaxios@gmail.com>
2023-04-19 20:04:23 -0400

    Handling Exceptions Errors 	    - Creating ValidationBehavior class to handle Exceptions 	    - Creating ValidationException class to work with many or no 	      errors

---
+++ Application/Exceptions/ValidationExceptions.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+
+namespace Application.Exceptions
+{
+    public class ValidationExceptions : Exception
+    {
+        public ValidationExceptions() : base("Se han producido uno o más errores de validación")
+        {
+            Errors = new List<string>();
+        }
+        public List<string> Errors { get; }
+
+        public ValidationExceptions(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Errors.Add(failure.ErrorMessage);
+            }
+        }
+    }
+}
