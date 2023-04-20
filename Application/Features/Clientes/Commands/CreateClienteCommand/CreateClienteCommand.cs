91a961b883079c844c5060e3c3ed0e6973a0e708
Ignacio Ainol <iggnaxios@gmail.com>
2023-04-20 12:58:41 -0400

    Created Entities and Response Wrapper 	    - Created Client Entity 	    - Created Base Entity to inherit its properties 	    - Created Response Wrapper to send the data and message

---
+++ Application/Features/Clientes/Commands/CreateClienteCommand/CreateClienteCommand.cs
@@ -0,0 +1,15 @@
+﻿using Application.Wrappers;
+using MediatR;
+
+namespace Application.Features.Clientes.Commands.CreateClienteCommand
+{
+    public class CreateClienteCommand : IRequest<Response<int>>
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+    }
+}
---
+++ Domain/Common/AuditableBaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Common
+{
+    public abstract class AuditableBaseEntity
+    {
+        public virtual int Id { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime Created { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}
