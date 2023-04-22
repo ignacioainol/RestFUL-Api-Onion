61f90051fa1cf0f39b20a1c8ed358548ab87c452
Ignacio Ainol <iggnaxios@gmail.com>
2023-04-21 23:17:39 -0400

    Versioning and create api controller Cliente

--- Application/Features/Clientes/Commands/CreateClienteCommand/CreateClienteCommand.cs
+++ Application/Features/Clientes/Commands/CreateClienteCommand/CreateClienteCommand.cs
@@ -1,4 +1,7 @@
-﻿using Application.Wrappers;
+﻿using Application.Interfaces;
+using Application.Wrappers;
+using AutoMapper;
+using Domain.Entities;
 using MediatR;
 
 namespace Application.Features.Clientes.Commands.CreateClienteCommand
@@ -14,9 +17,20 @@ namespace Application.Features.Clientes.Commands.CreateClienteCommand
     }
     public class CreateClienteCommandHandler : IRequestHandler<CreateClienteCommand, Response<int>>
     {
+        private readonly IRepositoryAsync<Cliente> _repositoryAsync;
+        private readonly IMapper _mapper;
+
+        public CreateClienteCommandHandler(IRepositoryAsync<Cliente> repositoryAsync, IMapper mapper)
+        {
+            _repositoryAsync = repositoryAsync;
+            _mapper = mapper;
+        }
         public async Task<Response<int>> Handle(CreateClienteCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var nuevoRegistro = _mapper.Map<Cliente>(request);
+            var data = await _repositoryAsync.AddAsync(nuevoRegistro);
+
+            return new Response<int>(data.Id);
         }
     }
 }
---
+++ WebAPI/Controllers/v1/ClienteController.cs
@@ -0,0 +1,15 @@
+﻿using Application.Features.Clientes.Commands.CreateClienteCommand;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class ClienteController : BaseApiController
+    {
+        //POST api/<controller>
+        public async Task<IActionResult> Post(CreateClienteCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
