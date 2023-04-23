46693ccd87ca9b4b56eb099dc7fcd82ca116823b
Ignacio Ainol <iggnaxios@gmail.com>
2023-04-23 19:42:05 -0400

    Get Record By Id

---
+++ Application/DTOs/ClienteDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.DTOs
+{
+    public class ClienteDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int Edad { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+    }
+}
